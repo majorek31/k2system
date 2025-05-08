96b2ba8673987fe1d115b7a387859431b1b713b6
luenlito <aleksanderhansel00@gmail.com>
2025-05-08 12:41:20 +0200

    Make routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import LandingPage from "./components/landingPage";
 import NavBar from "./components/NavBar";
+
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import DeliveryPage from "./pages/DeliveryPage"
+import MainPage from "./pages/MainPage"
+import RegisterPage from "./pages/RegisterPage"
+import ServicePage from "./pages/ServicePage"
+import SettingsPage from "./pages/SettingsPage"
+import ShopPage from "./pages/ShopPage"
 
 function App() {
   const [scrollY, setScrollY] = useState(0);
@@ -19,12 +30,24 @@ function App() {
   return (
     <div className="scroll-smooth">
       <LandingPage setHeight={setHeight} height={height} />
-      <NavBar
-        scrollY={scrollY}
-        height={height}
-        setShowNavBar={setShowNavBar}
-        showNavBar={showNavBar}
-      />
+      <BrowserRouter>
+        <NavBar
+          scrollY={scrollY}
+          height={height}
+          setShowNavBar={setShowNavBar}
+          showNavBar={showNavBar}
+        />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/delivery" element={<DeliveryPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/service" element={<ServicePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
+      </BrowserRouter>
       <div className="h-1000">
         {/* content later */}
         {/* https://examples.motion.dev/ */}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,99 +1,114 @@
 import React, { useState } from "react";
 import { Fragment, useEffect } from "react";
-import SingleBar from "./SingleBar";
 import { useTheme } from "../hooks/useTheme";
+import SingleBar from "./SingleBar";
 
 export default function NavBar({ scrollY, height, showNavBar, setShowNavBar }) {
-  const [showContentForNav, setShowContentForNav] = useState(false);
+    const [showContentForNav, setShowContentForNav] = useState(false);
 
-  const { bGcolor } = useTheme();
+    const { bGcolor } = useTheme();
 
-  // check if navbar schould be visible
+    // check if navbar schould be visible
 
-  useEffect(() => {
-    scrollY > height ? setShowNavBar(true) : setShowNavBar(false),
-      setShowContentForNav(false);
-  }, [scrollY, height]);
+    useEffect(() => {
+        scrollY >= height ? setShowNavBar(true) : setShowNavBar(false), setShowContentForNav(false);
+    }, [scrollY, height]);
 
-  return (
-    <Fragment>
-      <div
-        className={`flex h-fit w-full flex-col transition-all duration-500 ease-in-out ${showNavBar ? "fixed top-0 translate-y-0 opacity-100" : "fixed top-0 -translate-y-full opacity-0"}`}
-      >
-        <div
-          className={`w-[100% -24px] m-3 h-fit rounded-xl ${bGcolor} shadow-lg`}
-        >
-          <div className="float-left flex items-center justify-center p-3">
-            <img
-              src="/icons/menu.svg"
-              alt="menu"
-              className="object-contain brightness-0 invert filter"
-              onClick={() => setShowContentForNav(!showContentForNav)}
-            />
-          </div>
-          <div className="float-right">{/* logo */}</div>
-        </div>
-        <div
-          className={`transition-all duration-500 ease-in-out ${showContentForNav ? "opacity-100" : "opacity-0"}`}
-        >
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"0ms"}
-            v2={"0ms"}
-          >
-            Strona głowna
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"150ms"}
-            v2={"25ms"}
-          >
-            Dostawa
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"300ms"}
-            v2={"50ms"}
-          >
-            O nas
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"400ms"}
-            v2={"75ms"}
-          >
-            Rejestracja
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"500ms"}
-            v2={"100ms"}
-          >
-            Usługi
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"600ms"}
-            v2={"125ms"}
-          >
-            Sklep
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"700ms"}
-            v2={"150ms"}
-          >
-            Kontakt
-          </SingleBar>
-          <SingleBar
-            showContentForNav={showContentForNav}
-            v1={"800ms"}
-            v2={"175ms"}
-          >
-            Ustawienia
-          </SingleBar>
-        </div>
-      </div>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <div
+                className={`flex h-fit w-full flex-col transition-all duration-500 ease-in-out ${showNavBar ? "fixed top-0 translate-y-0 opacity-100" : "fixed top-0 -translate-y-full opacity-0"}`}
+            >
+                <div
+                    className={`w-[100% -24px] m-3 h-fit rounded-xl ${bGcolor} shadow-lg`}
+                >
+                    <div className="float-left flex items-center justify-center p-3">
+                        <img
+                            src="/icons/menu.svg"
+                            alt="menu"
+                            className="object-contain brightness-0 invert filter"
+                            onClick={() => setShowContentForNav(!showContentForNav)}
+                        />
+                    </div>
+                    <div className="float-right">{/* logo */}</div>
+                </div>
+                <div
+                    className={`transition-all duration-500 ease-in-out ${showContentForNav ? "opacity-100" : "opacity-0"}`}
+                >
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"0ms"}
+                        v2={"0ms"}
+                        where={"/"}
+                        height={height}
+                    >
+                        Strona Główna
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"150ms"}
+                        v2={"25ms"}
+                        where={"/delivery"}
+                        height={height}
+                    >
+                        Dostawa
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"300ms"}
+                        v2={"50ms"}
+                        where={"/about"}
+                        height={height}
+                    >
+                        O nas
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"400ms"}
+                        v2={"75ms"}
+                        where={"/register"}
+                        height={height}
+                    >
+                        Rejestracja
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"500ms"}
+                        v2={"100ms"}
+                        where={"/service"}
+                        height={height}
+                    >
+                        Usługi
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"600ms"}
+                        v2={"125ms"}
+                        where={"/shop"}
+                        height={height}
+                    >
+                        Sklep
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"700ms"}
+                        v2={"150ms"}
+                        where={"/contact"}
+                        height={height}
+                    >
+                        Kontakt
+                    </SingleBar>
+                    <SingleBar
+                        showContentForNav={showContentForNav}
+                        v1={"800ms"}
+                        v2={"175ms"}
+                        where={"/settings"}
+                        height={height}
+                    >
+                        Ustawienia
+                    </SingleBar>
+                </div>
+            </div>
+        </Fragment>
+    );
 }
